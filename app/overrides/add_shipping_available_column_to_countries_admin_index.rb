5f7949dca982c839d48f6e65126216d2b949aa85
Edwin Horneij <edwin@astekwallcovering.com>
2016-05-18 17:33:57 -0700

    Correcting typo in filename, adding original to definition.

--- app/overrides/add_shipping_available_column_to_countries_admin_index.rb
+++ app/overrides/add_shipping_available_column_to_countries_admin_index.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(
+    :original => 'bdef8b34fd0cba967cd73dbbc3f0d820cd7b73f6',
     :virtual_path => 'spree/admin/countries/index',
     :name => 'add_shipping_available_header_to_countries_admin_index',
     :insert_before => 'tr[data-hook="tax_header"] th:last-child',
@@ -6,6 +7,7 @@ Deface::Override.new(
 )
 
 Deface::Override.new(
+    :original => '7a17e2d58a09927ab4903b6109fb53e52fd3c5d8',
     :virtual_path => 'spree/admin/countries/index',
     :name => 'add_shipping_available_cells_to_countries_admin_index',
     :insert_before => 'tr[data-hook="country_row"] td:last-child',
