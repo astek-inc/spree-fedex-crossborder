0f56def7d6aa1b69c552f7c172dc380dd67cb920
Edwin Horneij <edwin@astekwallcovering.com>
2016-05-24 16:10:05 -0700

    Converting prices to current_currency for display purposes.

---
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -0,0 +1,19 @@
+Spree::FrontendHelper.class_eval do
+
+    def link_to_cart(text = nil)
+
+      text = text ? h(text) : Spree.t('cart')
+      css_class = nil
+
+      if simple_current_order.nil? or simple_current_order.item_count.zero?
+        text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{Spree.t('empty')})"
+        css_class = 'empty'
+      else
+        text = "<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{simple_current_order.item_count})  <span class='amount'>#{convert_display_price(simple_current_order.total)}</span>"
+        css_class = 'full'
+      end
+
+      link_to text.html_safe, spree.cart_path, :class => "cart-info #{css_class}"
+    end
+
+end
