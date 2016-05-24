2313dd6555af022c353e0f5066ce89af61b6cb36
Edwin Horneij <edwin@astekwallcovering.com>
2016-05-24 16:07:17 -0700

    Overloading Spree.fetch_cart function to prevent caching.

--- app/assets/javascripts/spree/frontend/spree_fedex_crossborder.js
+++ app/assets/javascripts/spree/frontend/spree_fedex_crossborder.js
@@ -1,6 +1,21 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 
+(function() {
+
+    Spree.fetch_cart = function() {
+        return $.ajax({
+            url: Spree.pathFor("cart_link"),
+            success: function(data) {
+                return $('#link-to-cart').html(data);
+            },
+            cache: false
+        });
+    };
+
+}).call(this);
+
+
 // var DYW = DYW || {};
 //
 // DYW.currency = {
