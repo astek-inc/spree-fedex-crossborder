c03204e6e66ab091199ba555a526cfb6da714450
Edwin Horneij <edwin@astekwallcovering.com>
2017-06-27 16:22:35 -0700

    Clear cache after changing country, because there is sample ordering information on the product pages that only appears for US orders

--- app/controllers/spree/countries_controller.rb
+++ app/controllers/spree/countries_controller.rb
@@ -18,6 +18,8 @@ module Spree
         flash[:danger] = 'Unable to change country.'
       end
 
+      set_no_cache_headers
+
       unless session[:return_to].nil?
         redirect_to session.delete(:return_to)
       else
@@ -28,6 +30,15 @@ module Spree
 
     def find_by_iso
       render :json => Spree::Country.where(iso: params[:iso]).take
+    end
+
+    private
+
+    # Clear cache because there is sample ordering information on the product pages that only appears for US orders
+    def set_no_cache_headers
+      response.headers['Cache-Control'] = 'no-cache, no-store'
+      response.headers['Pragma'] = 'no-cache'
+      response.headers['Expires'] = 'Thu, 01 Jan 1970 00:00:00 GMT'
     end
 
   end
